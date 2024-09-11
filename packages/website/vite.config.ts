97636d81823bf4f8e20935f140d0127849c6c1f1
Rob Marscher <rob@robmarscher.com>
2024-09-11 14:36:12 -0400

    Add sst to vite external list

--- packages/website/vite.config.ts
+++ packages/website/vite.config.ts
@@ -1,10 +1,17 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { fileURLToPath } from "node:url";
+import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig(() => {
 	return {
+		ssr: {
+			// vite tries to bundle the whole thing for SSR by default
+			external: [
+				"sst",
+				/// append-vite-external-slot
+			],
+		},
 		plugins: [
 			react({
 				/// start-react-compiler-slot
